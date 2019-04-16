3813a0bca139d1871ebc961018dfd0d49c15d1bb
suskan <49642585+suskan@users.noreply.github.com>
2019-04-16 10:36:03 +0700

    Update line2.php

--- line2.php
+++ line2.php
@@ -11,7 +11,7 @@ $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
-if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
+if($arrJson['events'][0]['message']['text'] == "สวัสดี" OR "ดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
